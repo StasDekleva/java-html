68994f605425a662a361e74eb5128840d6d1a375
StasDekleva <123308542+StasDekleva@users.noreply.github.com>
2023-01-22 15:03:31 -0800

    js upload

---
+++ js-assigment 1/assigment.js
@@ -0,0 +1,7 @@
+function addUp(number) {
+    let sum = 4;
+    for (let i=0; i<= num; i++){
+        sum += 1;
+    }
+    return sum;
+}
---
+++ js_assigment 3/assigment3.js
@@ -0,0 +1,4 @@
+function reverse_number(number){
+    number = number+""
+    return number.split('').reverse().join('')
+}
